5c42b60db7fd4f215a1a33d6fc2ad2c09b513bcd
nikhilpakhloo <nikhilpakhloo@gmail.com>
2023-09-25 14:57:52 +0530

    final commit

--- src/routeHorizontal/work/Work.jsx
+++ src/routeHorizontal/work/Work.jsx
@@ -7,44 +7,45 @@ export default function Work() {
         <div className="card">
 
           <h5>Weather App -Reactjs</h5>
-         
+
           <a href="https://weather-app-alpha-ivory.vercel.app/" target="_blank"> <button >Check Out</button></a>
 
         </div>
         <div className="card">
           <h5>TextUtils app - Reactjs</h5>
           <a href="https://utils-five.vercel.app/" target="_blank"> <button >Check Out</button></a>
-        
 
-        </div>
-        <div className="card">
-          <h5>QRCodeGenerator-reactjs</h5>
-          <a href="https://qrcode-delta-lyart.vercel.app/" target="_blank"> <button >Check Out</button></a>
-         
-
-        </div>
-        <div className="card">
-          <h5>Threejs-app</h5>
-          <a href="https://applephone-w9mj.vercel.app/" target="_blank"> <button>Check Out</button></a>
-       
-
-        </div>
-        <div className="card">
-          <h5>Nextjs-CRUDAPP</h5>
-          <a href="https://nextjs-crud-beta.vercel.app/" target="_blank"> <button>Check Out</button></a>
-       
 
         </div>
         <div className="card">
           <h5>ZohoCRM integration</h5>
           <a href="https://zoho-crm-crud.vercel.app/" target="_blank"> <button>Check Out</button></a>
-       
+
 
         </div>
-      
-       
+        <div className="card">
+          <h5>QRCodeGenerator-reactjs</h5>
+          <a href="https://qrcode-delta-lyart.vercel.app/" target="_blank"> <button >Check Out</button></a>
+
+
+        </div>
+        <div className="card">
+          <h5>Threejs-app</h5>
+          <a href="https://applephone-w9mj.vercel.app/" target="_blank"> <button>Check Out</button></a>
+
+
+        </div>
+        <div className="card">
+          <h5>Nextjs-CRUDAPP</h5>
+          <a href="https://nextjs-crud-beta.vercel.app/" target="_blank"> <button>Check Out</button></a>
+
+
+        </div>
+
+
+
       </div>
-   
+
     </div>
   )
 }
